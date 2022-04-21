8cf517663983b0baf8e7c420f07afe345647a45e
Alberto Bueno <abuenop@hotmail.com>
2022-04-21 06:17:42 -0500

    Day_2 - Resta dos numeros enteros e imprime el resultado

---
+++ Day_2.py
@@ -0,0 +1,8 @@
+# 100DiasDePython
+# Dia_2
+# Resta dos numeros enteros e imprime el resultado 
+
+minuend = int(input("Ingrese el minuendo: "))
+susbtrahend = int(input("Ingrese el sustraendo:"))
+difference = minuend-susbtrahend
+print("El valor de la resta es:", difference)
