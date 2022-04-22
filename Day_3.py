2bfd91f426b39966efaa6de1f744506d7419c9a1
Alberto Bueno <abuenop@hotmail.com>
2022-04-22 05:18:16 -0500

    Dia 3 Se dDeclara uaa variable de tipo cadena que tenga un texto divertido e imprimela

---
+++ Day_3.py
@@ -0,0 +1,20 @@
+# 100DiasDePython
+# Dia_3 
+# Declara uaa variable de tipo cadena que tenga un texto divertido e imprimela
+
+fraseDivertida = "\"Yo tenia un problema, \n yo decidi escribir un programa, \n Ahora tengo 1 problema, 9 errores y 12 warnings. XD\""
+print (fraseDivertida)
+
+# Podemos usar comillas dentro de comillas en Python
+# usando el operador de escape o el carácter \. 
+# El operador de escape se usa para escapar o ignorar un carácter dentro de una cadena. 
+# Por lo tanto, podemos usarlo para nuestro caso de uso
+#
+# a = "Double Quotation Marks: \"\""
+# b = 'Single Quotation Marks: \'\''
+# print(a)
+# print(b)
+#
+# Resultado
+# Double Quotation Marks: ""
+# Single Quotation Marks: ''
