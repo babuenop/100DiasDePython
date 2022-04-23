a3a748d461f09db0ac0b10373cf71c7d9191cd70
Alberto Bueno <abettetto@gmail.com>
2022-04-23 07:18:37 -0500

    Day_4 - Usa la funcion Type() para imprimir el tipo de una variable booleana

---
+++ Day_4.py
@@ -0,0 +1,13 @@
+# 100DiasDePython
+# Dia_4
+# Usa la funcion Type() para imprimir el tipo de una variable booleana
+
+verdadero = False
+falso = False
+print (type(verdadero))
+print (type(falso))
+
+## if type(verdadero) is bool:
+##    print ("Es Booleana")
+## else:
+##    print("No es Booleana")
