fb34b4d288958fc2cdb34d8a0e8bc7b91502a30a
Alberto Bueno <abettetto@gmail.com>
2022-04-24 06:44:16 -0500

    Day 5 - Declara una lista de numeros y encuentra el valor mas pequeño

---
+++ Day_5.py
@@ -0,0 +1,16 @@
+# 100DiasDePython
+# Dia_5
+# Declara una lista de numeros y encuentra el valor mas pequeño
+
+lista = [5,-4,0,2,1,-3]
+menor=lista[0]
+
+for i in lista:
+    if lista[i]<menor:
+        menor=lista[i]
+print(menor)
+
+
+
+
+
