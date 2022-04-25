1792a1322dfbf0c5d1e3521af124b60c12e8becc
Alberto Bueno <abettetto@gmail.com>
2022-04-25 06:51:00 -0500

    Declara 2 variables de tipo cadena, concatenalas e imprime el resultado

---
+++ Day_6.py
@@ -0,0 +1,8 @@
+# 100DiasDePython
+# Dia_6
+# Declara 2 variables de tipo cadena, concatenalas e imprime el resultado. 
+
+nombre = "Alberto"
+apellido = "Bueno"
+nombre_completo = nombre+" "+apellido
+print(nombre_completo)
