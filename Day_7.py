753be4611b76e7a41bcd046562e013c8b602c57d
Alberto Bueno <abettetto@gmail.com>
2022-04-26 06:23:10 -0500

    Declara una variable tipo cadena con saltos de linea e imprimela

---
+++ Day_7.py
@@ -0,0 +1,6 @@
+# 100DiasDePython
+# Dia_7
+# Declara una variable tipo cadena con saltos de linea e imprimela 
+
+poema = "Dia 7, \nEstamos imprimiendo \nsaltos de linea en Python, \n"
+print(poema)
