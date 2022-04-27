09a2eea82d1389857628df57c6f19b4926182d11
Alberto Bueno <abettetto@gmail.com>
2022-04-27 06:23:32 -0500

    Declarar una lista de numeros e imprimir el numero de elementos

---
+++ Day_8.py
@@ -0,0 +1,8 @@
+# 100DiasDePython
+# Dia_8
+# Declara una lista de numeros e imprime el numero de elementos. 
+
+lista = [1,"X",3,4,5,6,7,8,9,10]
+num_elementos = len(lista)
+print(lista)
+print("La lista anterior contiene ", num_elementos ," elementos")
