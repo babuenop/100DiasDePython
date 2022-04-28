8d425669ba6a863f37cb6b8fa3b15ea38614acea
Alberto Bueno <abettetto@gmail.com>
2022-04-28 18:20:35 -0500

    Evalua una expresion para ver si un numero es mayor que otro e imprime el resultado

---
+++ Day_9.py
@@ -0,0 +1,17 @@
+# 100DiasDePython
+# Dia_9
+# Evalua una expresion para ver si un numero es mayor que otro e imprime el resultado0
+# Alberto Bueno (babuenop)#6398
+
+numero_1 = 300
+numero_2 = 250
+
+if numero_1 > numero_2:
+    print ("El numero mayor es:", numero_1)
+else:
+    print ("El numero mayor es:", numero_2)
+    
+
+    
+    
+
---
+++ test.py
@@ -0,0 +1,6 @@
+Ist = []
+for i in range(2,-1,-1):
+    for j in range(6,9,2):
+        Ist.append(i+j)
+        print(Ist)
+print(Ist)
