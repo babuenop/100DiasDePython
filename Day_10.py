02869b1ea2016fdcd15d7cc69a4b903165a01172
Alberto Bueno <abettetto@gmail.com>
2022-04-29 06:31:22 -0500

    Inicializa 3 variables numericas de forma multiple e imprime sus valores en una sola linea

---
+++ Day_10.py
@@ -0,0 +1,8 @@
+# 100DiasDePython
+# Dia_10
+# Inicializa 3 variables numericas de forma multiple e imprime sus valores en una sola linea 
+# Alberto Bueno (babuenop)#6398
+
+a, b, c = 0, 4, 5
+print(a, b, c)
+
