78cb076ca0c30f7aedfb2454bcdb43f8c80fe8ad
Alberto Bueno <abettetto@gmail.com>
2022-04-30 06:31:46 -0500

    Declara una variable numerica y una de tipo cadena, intercambia los valores de ambas e imprime el resultado en una sola linea

---
+++ Day_11.py
@@ -0,0 +1,10 @@
+# 100DiasDePython
+# Dia_10
+# Declara una variable númerica y una de tipo cadena, intercambia los valores de ambas e imprime el resultado en una sola linea. 
+# Alberto Bueno (babuenop)#6398
+
+a=10
+b="Hola"
+a , b = b , a
+print(a , b)
+
