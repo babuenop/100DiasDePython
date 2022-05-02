aecb32299400321299066203fbf416c996aad292
Alberto Bueno <abettetto@gmail.com>
2022-05-02 15:01:48 -0500

    Declara dos variables booleanas, evalua una expresion logica usando los operadores logicos e imprime el resultado

---
+++ Day_13.py
@@ -0,0 +1,10 @@
+# 100DiasDePython
+# Dia_13
+# Declara dos variables booleanas, evalua una expresion logica usando los operadores logicos e imprime el resultado 
+# Alberto Bueno (babuenop)#6398
+
+a = True
+b = False
+expresion = a and not b 
+print(expresion)
+
