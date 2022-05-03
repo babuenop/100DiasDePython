15fd3197bacd9fa9314e08da5202af47096a3390
Alberto Bueno <abettetto@gmail.com>
2022-05-03 06:17:39 -0500

    Declara una variable mediante una expresion aritmetica usando dos operadores aritmeticos e imprime el resultado

---
+++ Day_14.py
@@ -0,0 +1,7 @@
+# 100DiasDePython
+# Dia_14
+# Declara una variable mediante una expresion aritmetica usando dos operadores aritmeticos e imprime el resultado. 
+# Alberto Bueno (babuenop)#6398
+
+promedio = (8 + 4) / 2
+print (promedio)
