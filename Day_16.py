73f0362039d664cc576b2c89d64f99412c66d334
Alberto Bueno <abettetto@gmail.com>
2022-05-05 05:23:39 -0500

    Declara una variable de tipo cadena e imprime la cantidad de caracteres de la cadena sin usar ciclos

---
+++ Day_16.py
@@ -0,0 +1,7 @@
+# 100DiasDePython
+# Dia_16
+# Declara una variable de tipo cadena e imprime la cantidad de caracteres de la cadena sin usar ciclos
+# Alberto Bueno (babuenop)#6398
+
+variable = "Germany"
+print(len(variable))
