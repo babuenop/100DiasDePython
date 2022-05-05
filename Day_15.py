c16b13e6dcf3a850e3cb4ab0170c5bbbdcc1c85c
Alberto Bueno <abettetto@gmail.com>
2022-05-04 20:13:19 -0500

    Declara una variable de tipo cadena e imprime los ultimos 3 caracter de la cadena sin usar ciclos

---
+++ Day_15.py
@@ -0,0 +1,7 @@
+# 100DiasDePython
+# Dia_15
+# Declara una variable de tipo cadena e imprime los ultimos 3 caracter de la cadena sin usar ciclos. 
+# Alberto Bueno (babuenop)#6398
+
+variable = "Germany"
+print(variable[-3:])
