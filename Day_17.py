0cc76353ad9b7744d89a1a8ca9a623cf7945c8d6
Alberto Bueno <abettetto@gmail.com>
2022-05-06 19:20:35 -0500

    Declara una variable de tipo cadena, encuentra la cantidad de veces que aparece la letra a

---
+++ Day_17.py
@@ -0,0 +1,7 @@
+# 100DiasDePython
+# Dia_16
+# Declara una variable de tipo cadena, encuentra la cantidad de veces que aparece la letra a
+# Alberto Bueno (babuenop)#6398
+
+variable = "Vamos a contar las letras a que aparecen en esta cadena"
+print(variable.count('a'))
