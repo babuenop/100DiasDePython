e46b04a04338eb190c388032fb322117bd414106
Alberto Bueno <abettetto@gmail.com>
2022-05-07 20:58:36 -0500

    Declara una variable de tipo cadena, encuentra el primer y ultimo caracter en orden lexicografico sin usar ciclos e imprimelos

---
+++ Day_18.py
@@ -0,0 +1,8 @@
+# 100DiasDePython
+# Dia_18
+# Declara una variable de tipo cadena, encuentra el primer y ultimo caracter en orden lexicografico sin usar ciclos e imprimelos
+# Alberto Bueno (babuenop)#6398
+
+cadena = "españa"
+a = (cadena[0],cadena[-1])
+print(sorted(a))
