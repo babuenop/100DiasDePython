fd42d13d46c52c5884eab7efd965ac1b9142c21c
Alberto Bueno <abettetto@gmail.com>
2022-05-08 21:38:15 -0500

    Declara una variable de tipo cadena, reviertela sin usar funciones adicionales e imprime el resultado.

---
+++ Day_19.py
@@ -0,0 +1,7 @@
+# 100DiasDePython
+# Dia_18
+# Declara una variable de tipo cadena, reviertela sin usar funciones adicionales e imprime el resultado. 
+# Alberto Bueno (babuenop)#6398
+
+cadena="Sigo aprendiendo Python"
+print(cadena[::-1])
