661c1f87067e6f1bd9c8611c5bb9c4b0220fc5b8
Alberto Bueno <abettetto@gmail.com>
2022-05-09 22:01:41 -0500

    Separa Mayusculas y Minusculas de la cadena

---
+++ Day_20.py
@@ -0,0 +1,13 @@
+# 100DiasDePython
+# Dia_20
+# De la siguiente cadena: 'PpYyTtHhOoNnIiSsTtAa' separa las mayusculas y minusculas 
+# sin usar ciclos en nuevas cadenas e imprime el resultado en una sola linea. 
+# Alberto Bueno (babuenop)#6398
+
+s="PpYyTtHhOoNnIiSsTtAa"
+lower=filter(str.islower,s)
+upper=filter(str.isupper,s)
+s1="".join(lower)
+s2="".join(upper)
+print (s1 +" "+ s2)
+#Output: pythonista PYTHONISTA
