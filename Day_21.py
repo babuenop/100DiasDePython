a873ecdf7d69eb1d70bb5b4dad8feb15dfef7edd
Alberto Bueno <abettetto@gmail.com>
2022-05-10 17:41:50 -0500

    Utiliza lo aprendido en el reto anterior para encontrar el mensaje escondido en hjfacetiluzislcafiesdolavedfiedesno git status

---
+++ Day_21.py
@@ -0,0 +1,10 @@
+
+# 100DiasDePython
+# Dia_21
+# Utiliza lo aprendido en el reto anterior para encontrar el mensaje escondido en
+# "hjfacetiluzislcafiesdolavedfiedesno"
+# Alberto Bueno (babuenop)#6398
+
+a="hjfacetiluzislcafiesdolavedfiedesno"
+mensaje=a[2::3]
+print(mensaje)
