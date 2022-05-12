c7f2f0db1ba73166fb6af786e8484e0c71ca9d70
Alberto Bueno <abettetto@gmail.com>
2022-05-11 19:03:24 -0500

    Utiliza las funciones cadenas para crear una lista de palabras con la siguiente cadena dada

---
+++ Day_22.py
@@ -0,0 +1,8 @@
+# 100DiasDePython
+# Dia_21
+# Utiliza las funciones cadenas para crear una lista de palabras con la siguiente cadena:
+# cadena = "Los pequeños pasos te llevan a algo más grande"
+# Alberto Bueno (babuenop)#6398
+
+cadena = "Los pequeños pasos te llevan a algo más grande"
+print(cadena.split())
