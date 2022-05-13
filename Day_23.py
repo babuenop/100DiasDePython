7f51127005fd3652adb2184f92e5193e7eca5066
Alberto Bueno <abettetto@gmail.com>
2022-05-12 22:02:53 -0500

    Utiliza las funciones cadenas para quitar los espacios innecesarios de la cadena dada

---
+++ Day_23.py
@@ -0,0 +1,8 @@
+# 100DiasDePython
+# Dia_21
+# Utiliza las funciones cadenas para quitar los espacios innecesarios 
+# de la siguiente cadena. cadena = " Python es divertido "
+# Alberto Bueno (babuenop)#6398
+
+cadena = " Python es divertido "
+print(cadena.lstrip().rstrip())
