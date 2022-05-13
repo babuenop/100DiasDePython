50f9a29db93b22b9cf5639a6d8174cbde427f2f6
Braulio Alberto Bueno Pabon <bbuenop@pa.cirsa.com>
2022-05-13 15:19:27 -0500

    Crea una lista de compras e imprime del 2do al 4to elemento

---
+++ Day_24.py
@@ -0,0 +1,7 @@
+# 100DiasDePython
+# Dia_24
+# Crea una lista de compras e imprime del 2do al 4to elemento
+# Alberto Bueno (babuenop)#6398
+
+lista = ['Tomate', 'Pepino', 'Piña', 'Narajas', 'Leche', 'huevos']
+print(lista[1:4:1])
