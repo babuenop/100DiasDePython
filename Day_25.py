6cc53995f62cd4b8e670deb63fa4056ba6abc3d5
Alberto Bueno <abettetto@gmail.com>
2022-05-14 18:57:03 -0500

    Utiliza la lista de compras del reto anterior para contruir una cadena con saltos de linea

---
+++ Day_25.py
@@ -0,0 +1,9 @@
+# 100DiasDePython
+# Dia_24
+# Utiliza la lista de compras del reto anterior para contruir una cadena con saltos de linea. 
+# sin usar ciclos e imprime el resultado
+# Alberto Bueno (babuenop)#6398
+
+lista = ['Tomate', 'Pepino', 'Piña', 'Narajas', 'Leche', 'huevos']
+str= lista[0] + '\n' + lista[1] + '\n' + lista[2] + '\n' + lista[3] + '\n' +lista[4]
+print(str)
