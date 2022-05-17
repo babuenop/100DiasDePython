4e3a2fe1aee6004f00de716d3abd0cb10c15baab
Alberto Bueno <abettetto@gmail.com>
2022-05-16 21:25:41 -0500

    Utiliza la lista de compras del reto anterior para agregarle un nuevo elemento al final e imprime la lista.

---
+++ Day_27.py
@@ -0,0 +1,15 @@
+''' 
+100DiasDePython - Dia_27   
+------------------------
+
+Utiliza la lista de compras del reto anterior para agregarle un nuevo elemento 
+al final e imprime la lista. 
+Alberto Bueno (babuenop)#6398 
+
+'''
+
+lista = ['Tomate', 'Pepino', 'Piña', 'Narajas', 'Leche', 'huevos']
+lista.append("Pasta")
+print(lista)
+
+
