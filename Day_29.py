22bce72f1f92ccecf4e75f39a192ad697b07e30d
Alberto Bueno <abettetto@gmail.com>
2022-05-18 17:57:39 -0500

    Utiliza la lista de compras de compras del reto anterior para remover todos los elementos de la lista usando una funcion de listas e imprime el resultado

---
+++ Day_29.py
@@ -0,0 +1,11 @@
+''' 
+100DiasDePython - Dia_29
+------------------------
+Utiliza la lista de compras de compras del reto anterior para remover todos los elementos de la lista usando una funcion de listas
+e imprime el resultado 
+Alberto Bueno (babuenop)#6398 
+
+'''
+lista = ['Tomate', 'Pepino', 'Piña', 'Narajas', 'Leche', 'huevos']
+lista.clear()
+print(lista)
