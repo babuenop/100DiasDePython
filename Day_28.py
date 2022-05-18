5e7a3dd4912d1cfb7db38a9ae3bfcb8361d9cef6
Alberto Bueno <abettetto@gmail.com>
2022-05-17 22:05:33 -0500

    Utiliza la lista de compras de compras del reto anterior para remover el primer elemento de la lista sin usar ciclos e imprime la lista.

---
+++ Day_28.py
@@ -0,0 +1,12 @@
+''' 
+100DiasDePython - Dia_28   
+------------------------
+Utiliza la lista de compras de compras del reto anterior para remover el primer elemento de la lista. 
+Sin usar ciclos e imprime la lista. 
+Alberto Bueno (babuenop)#6398 
+
+'''
+
+lista = ['Tomate', 'Pepino', 'Piña', 'Narajas', 'Leche', 'huevos']
+lista.pop(0)
+print(lista)
