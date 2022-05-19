a75f678d3623173c427168c79837455bf9e99ee0
Alberto Bueno <abettetto@gmail.com>
2022-05-19 06:08:40 -0500

    Crea una lista de animales y ordénala automaticamente usando funciones de listas. Imprime el resultado.

---
+++ Day_30.py
@@ -0,0 +1,11 @@
+''' 
+100DiasDePython - Dia_30
+------------------------
+Crea una lista de animales y ordénala automaticamente usando 
+funciones de listas. Imprime el resultado. 
+Alberto Bueno (babuenop)#6398 
+
+'''
+lista = ['Dog', 'Cat', 'Jiraffe', 'Tiger', 'Lion', 'Bird']
+lista.sort()
+print(lista)
