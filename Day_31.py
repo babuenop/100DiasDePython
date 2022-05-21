2d42103fb8dd80d813da6ac44d922e4e2ed78747
Alberto Bueno <abettetto@gmail.com>
2022-05-20 21:33:57 -0500

    Declara dos valores boleanos, convierte una en entero y la otra en cadena. Imprime el resultado en una sola linea.

---
+++ Day_31.py
@@ -0,0 +1,12 @@
+''' 
+100DiasDePython - Dia_31
+------------------------
+Declara dos valores boleanos, convierte una en entero y la otra en cadena. 
+Imprime el resultado en una sola linea. 
+Alberto Bueno (babuenop)#6398 
+
+'''
+a = True 
+b = False 
+print(int(a), str(b))
+
