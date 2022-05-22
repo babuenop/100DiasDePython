2ebea6eec9b6481dcf6e312ad9f6b86d411ec5c8
Alberto Bueno <abettetto@gmail.com>
2022-05-21 20:09:10 -0500

    Declara dos listas, una vacia y la contra con elementos. Convierte ambas listas en booleanos. Imprime el resultado en una sola linea.

---
+++ Day_32.py
@@ -0,0 +1,13 @@
+''' 
+100DiasDePython - Dia_31
+------------------------
+Declara dos listas, una vacia y la contra con elementos. 
+Convierte ambas listas en booleanos. 
+Imprime el resultado en una sola linea. 
+Alberto Bueno (babuenop)#6398 
+
+'''
+a = []
+b = ['Tomate', 'Pepino', 'Sal']
+c , d = bool(a), bool(b)
+print(c , d)
