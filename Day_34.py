4c0c6664c9b3c65b9645ed03ffd05b5d1f987f6c
Alberto Bueno <abettetto@gmail.com>
2022-05-23 21:44:39 -0500

    Declara un diccionario de 5 palabras relacionadas a la programacion con su definicion

---
+++ Day_34.py
@@ -0,0 +1,17 @@
+''' 
+100 Dias De Python - Dia 34
+------------------------
+Declara un diccionario de 5 palabras relacionadas a la programacion con su definicion
+Imprime el resultado
+Alberto Bueno (babuenop)#6398 
+
+'''
+
+Words = {
+    "Atributo" : "Componente que tienen las clases para almacenar informacion",
+    "Booleano" : "Tipo de datos logico que se caracteriza porque unicamente debe tener dos valores, ó True ó False",
+    "Clase" : "Tipo de datos que define un conjunto de atributos y métodos", 
+    "Metodo" : "Componente de una clase que aporta funcionalidad a la misma",
+    "Tupla" : "Conjunto ordenado e inmutable de elementos. Las tuplas pueden contener elementos de diferentes tipos"
+    }
+print(Words)
