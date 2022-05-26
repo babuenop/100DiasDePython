90d91de1910cf1d86f4fbd3282c060c37081307e
Alberto Bueno <abettetto@gmail.com>
2022-05-25 23:10:21 -0500

    Eliminar el primer elemento del diccionario

---
+++ Day_36.py
@@ -0,0 +1,22 @@
+''' 
+100 Dias De Python - Dia 35
+------------------------
+Utiliza el diccionario de palabras del reto anterior para eliminar el primer elemento del diccionario 
+Imprime la cantidad de elementos del diccionario. 
+Alberto Bueno (babuenop)#6398 
+
+'''
+
+Words = {
+    "Atributo" : "Componente que tienen las clases para almacenar informacion",
+    "Booleano" : "Tipo de datos logico que se caracteriza porque unicamente debe tener dos valores, ó True ó False",
+    "Clase" : "Tipo de datos que define un conjunto de atributos y métodos", 
+    "Metodo" : "Componente de una clase que aporta funcionalidad a la misma",
+    "Tupla" : "Conjunto ordenado e inmutable de elementos. Las tuplas pueden contener elementos de diferentes tipos",
+    "Objeto" : "Instancia de una clase",
+    }
+
+del Words["Clase"]
+print(len(Words))
+
+
