a2db747f0e1a61beccb089e122f066a1e677e182
Alberto Bueno <abettetto@gmail.com>
2022-05-27 21:05:31 -0500

    Declara un conjunto de 5 animales e imprime el resultado.

---
+++ Day_38.py
@@ -0,0 +1,12 @@
+''' 
+100 Dias De Python - Dia 38
+------------------------
+Declara un conjunto de 5 animales e imprime el resultado. 
+Alberto Bueno (babuenop)#6398 
+
+'''
+
+Animales = {'Lion', 'Cat', 'Dog', 'Mouse', 'Elephant'}
+print(Animales)
+
+
