e0e35f8e9986f10f8b27f3a0f1016ad222e0004c
Alberto Bueno <abettetto@gmail.com>
2022-05-28 22:05:03 -0500

    Utiliza el conjunto del reto anterior y verifica si tienes un gato en el conjunto. Imprime el resultado.

---
+++ Day_39.py
@@ -0,0 +1,14 @@
+''' 
+100 Dias De Python - Dia 39
+------------------------
+Utiliza el conjunto del reto anterior y verifica si tienes un gato en el conjunto. 
+Imprime el resultado. 
+Alberto Bueno (babuenop)#6398 
+
+'''
+
+Animales = {'Lion', 'Cat', 'Dog', 'Mouse', 'Elephant'}
+cat_in_animales = "Cat" in Animales
+print(cat_in_animales)
+
+
