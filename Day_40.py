d8eab7e07d6d11396fd235a788dd78b46ed0dc65
Alberto Bueno <abettetto@gmail.com>
2022-05-29 21:39:27 -0500

    Utiliza el conjunto del reto anterior y elimina al gato del conjunto, si es que existiera, sin usar sentencias condicionales e imprime el resultado.

---
+++ Day_40.py
@@ -0,0 +1,14 @@
+''' 
+100 Dias De Python - Dia 40
+------------------------
+Utiliza el conjunto del reto anterior y elimina al gato del conjunto, 
+si es que existiera, sin usar sentencias condicionales e imprime el resultado. 
+Alberto Bueno (babuenop)#6398 
+
+'''
+
+Animales = {'Lion', 'Cat', 'Dog', 'Mouse', 'Elephant'}
+Animales.remove('Cat')
+print(Animales)
+
+
