142a0034f795795b679a6e17b4d3432acb95b1b1
Alberto Bueno <abettetto@gmail.com>
2022-05-30 23:27:20 -0500

    Utiliza el conjunto del reto anterior, define un uevo conjunto de mascotas. Encuentra su interseccion de ambos conjuntos sin usar ciclos e imprime el resultado.

---
+++ Day_41.py
@@ -0,0 +1,15 @@
+''' 
+100 Dias De Python - Dia 40
+------------------------
+Utiliza el conjunto del reto anterior, define un uevo conjunto de mascotas, 
+Encuentra su interseccion de ambos conjuntos sin usar ciclos e imprime el resultado. 
+Alberto Bueno (babuenop)#6398 
+
+'''
+
+Animales_1 = {'Lion', 'Cat', 'Dog', 'Mouse', 'Elephant'}
+Animales_2 = {'Cow', 'Ant', 'Dog', 'Mouse', 'Fish'}
+Animales = Animales_1 & Animales_2
+print(Animales)
+
+
