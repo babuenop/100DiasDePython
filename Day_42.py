ee2b610e3cbe3e52a211c9d3b7110880a863cd4f
Alberto Bueno <abettetto@gmail.com>
2022-05-31 22:22:57 -0500

    Utiliza el conjunto del reto anterior, para encontrar la union de ambos conjuntos sin usar ciclos e imprime el resultado

---
+++ Day_42.py
@@ -0,0 +1,14 @@
+''' 
+100 Dias De Python - Dia 40
+------------------------
+Utiliza el conjunto del reto anterior, para encontrar la union de ambos conjuntos sin usar ciclos e imprime el resultado
+Alberto Bueno (babuenop)#6398 
+
+'''
+
+Animales = {'Lion', 'Cat', 'Dog', 'Mouse', 'Elephant'}
+mascotas = {'Cow', 'Ant', 'Dog', 'Mouse', 'Fish'}
+union = Animales.union(mascotas)
+print(Animales)
+
+
