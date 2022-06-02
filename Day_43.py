9f1747fb3391c2ed8a11c316535a9476cf8d09cf
Alberto Bueno <abettetto@gmail.com>
2022-06-01 23:25:10 -0500

    Se declara una tupla con simbolos

---
+++ Day_43.py
@@ -0,0 +1,12 @@
+''' 
+100 Dias De Python - Dia 43
+------------------------
+Declara una tupla de 5 simbolos que representen magnitudes fisicas e imprime el resultado
+Alberto Bueno (babuenop)#6398 
+
+'''
+
+simbolos = ('ω','Ω','μ','Φ','α')
+print(simbolos)
+
+
