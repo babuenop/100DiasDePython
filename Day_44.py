ae1cc723d4af08f256ebf26f1f91d6fe839c2159
Alberto Bueno <abettetto@gmail.com>
2022-06-02 21:49:14 -0500

    Usa la tupla del ejemplo anterior e imprime los primeros 3 elementos

---
+++ Day_44.py
@@ -0,0 +1,12 @@
+''' 
+100 Dias De Python - Dia 44
+------------------------
+Usa la tupla del ejemplo anterior e imprime los primeros 3 elementos. 
+Alberto Bueno (babuenop)#6398 
+
+'''
+
+simbolos = ('ω','Ω','μ','Φ','α')
+print(simbolos[0:3])
+
+
