5b1343bb5c17ec039d36dccef6e29c50748f5f57
Alberto Bueno <abettetto@gmail.com>
2022-06-05 11:35:38 -0500

    Crea una lista de 5 numeros pares usando la funcion range() e imprime el resultado

---
+++ Day_47.py
@@ -0,0 +1,12 @@
+''' 
+100 Dias De Python - Dia 47
+------------------------
+Crea una lista de 5 numeros pares usando la funcion range() e imprime el resultado
+Alberto Bueno (babuenop)#6398 
+
+'''
+
+numeros = list(range(2,11,2))
+print(numeros)
+
+
