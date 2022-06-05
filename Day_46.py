d72852feb02299efceb75741331f9cddc978d910
Alberto Bueno <abettetto@gmail.com>
2022-06-04 22:20:20 -0500

    Crea una lista de 10 numeros usando la funcion range() e imprime el resultado

---
+++ Day_46.py
@@ -0,0 +1,12 @@
+''' 
+100 Dias De Python - Dia 46
+------------------------
+Crea una lista de 10 numeros usando la funcion range() e imprime el resultado
+Alberto Bueno (babuenop)#6398 
+
+'''
+
+numeros = list(range(0,10))
+print(numeros)
+
+
