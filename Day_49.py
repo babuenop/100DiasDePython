e30439aae79c0d18fadeceeb7f385512b127be24
Alberto Bueno <abettetto@gmail.com>
2022-06-07 22:47:44 -0500

    Declara una variable numerica con valor 100, decrementa su valor de 10 en 10 mientras no sea 0. Imprime el valor de la variable en cada iteracion.

---
+++ Day_49.py
@@ -0,0 +1,15 @@
+''' 
+100 Dias De Python - Dia 48
+------------------------
+Declara una variable numerica con valor 100, decrementa su valor de 10 en 10 mientras no sea 0. Imprime el valor de la variable en cada iteracion. 
+Alberto Bueno (babuenop)#6398 
+
+'''
+
+var = 100
+while var !=0:
+    print(var)
+    var = var - 10
+    
+    
+
