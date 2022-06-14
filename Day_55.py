e48e675b9856fc147df1d98534cba6d5b10bb99d
Alberto Bueno <abettetto@gmail.com>
2022-06-13 23:31:32 -0500

    Funcion recursiva para hacer una cuenta regresiva a 0

---
+++ Day_55.py
@@ -0,0 +1,35 @@
+''' 
+100 Dias De Python - Dia 55
+---------------------------
+'''
+
+def countdown (number):
+    '''
+    Funcion recursiva para hacer una cuenta regresiva a 0
+    '''
+    if number == 0: 
+        print (0)
+    else:
+        print (number)
+        countdown(number-1)
+
+countdown (5)
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    
+    
+
