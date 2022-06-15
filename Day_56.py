a7ea249efdbbee45ad8cd39936195477f0085e5a
Alberto Bueno <abettetto@gmail.com>
2022-06-14 22:52:47 -0500

    Funcion lambda ppara encontrar la raiz cuadrada de una lista de numeros

---
+++ Day_56.py
@@ -0,0 +1,24 @@
+''' 
+100 Dias De Python - Dia 56
+---------------------------
+'''
+import math
+
+my_list = [49,4,36,16,25]
+
+new_list = list(map(lambda x: math.sqrt(x) , my_list))
+
+print(new_list)
+
+
+
+
+
+
+
+
+
+    
+    
+    
+
