6383575e4daba2925079eb23cbe755929714d874
Alberto Bueno <abettetto@gmail.com>
2022-06-16 23:18:59 -0500

    Utiliza una funcion lambda para sumar las dos listas

---
+++ Day_58.py
@@ -0,0 +1,21 @@
+''' 
+100 Dias De Python - Dia 58
+---------------------------
+Utiliza una funcion lambda para sumar las dos listas.
+'''
+
+list_a = [2, 4, 5]
+list_b = [6, 7, 1]
+result = map(lambda x, y: x + y, list_a, list_b)
+print(list(result))
+
+
+
+
+
+
+
+    
+    
+    
+
