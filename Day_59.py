b9b1a18d0b9a8d1e60b91b4e6245bde15cf3108f
Alberto Bueno <abettetto@gmail.com>
2022-06-17 22:36:50 -0500

    Utiliza una funcion lambda para ordenar de forma ascendente luna lista de tuplas

---
+++ Day_59.py
@@ -0,0 +1,22 @@
+''' 
+100 Dias De Python - Dia 59
+---------------------------
+Utiliza una funcion lambda para ordenar de forma ascendente la siguiente lista de tuplas tomando en cuenta el valor numerico como referencia. 
+[("Quimica",88), ("Fisica",90)("Lenguaje",97)]
+Imprime el resultado
+'''
+
+list = [("Quimica",88), ("Fisica",90),("Lenguaje",97)]
+list.sort(key=lambda x:x[1], reverse=True)
+print(list)
+
+
+
+
+
+
+
+    
+    
+    
+
