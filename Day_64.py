b257b6e0a93ced0069a64a9574230a5f1f9f635b
Alberto Bueno <abettetto@gmail.com>
2022-06-23 00:25:28 -0500

    Se agrega regex correcto para remover 0 de las ips

--- Day_64.py
+++ Day_64.py
@@ -6,7 +6,9 @@ Imprime una lista con las IPs validas
 '''
 import re 
 
-ips = ['192.08.001.5','10.120.023.001','192.160.2.1']
-patron = "^\w*$"
-ips_validas = [c for c in ips if re.search(patron, c)]
-print(ips_validas)
+
+ip_adr  = ['192.08.001.5','10.120.023.001','192.160.2.1']
+validos = []
+for i in ip_adr:
+    validos.append(re.sub('\.[0]*', '.', i))
+print(validos)
