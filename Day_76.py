f0d26f2f861ac680cfdb16c2ae9b52cc7d2d5ce4
Alberto Bueno <abettetto@gmail.com>
2022-07-04 23:01:48 -0500

    Utiliza itertools para obtener el valor maximo de cada tupla de la lista

---
+++ Day_76.py
@@ -0,0 +1,10 @@
+''' 
+100 Dias De Python - Dia 76
+---------------------------
+Utiliza itertools para obtener el valor maximo de cada tupla de la lista
+Imprime el resultado en una lista
+'''
+import itertools
+lista = [(2,4,6),(7,8,5,6),(5,10)]
+maximos= list(max(lista) for lista, k in itertools.groupby(lista))
+print(maximos)
