6f78280a43d34656f1e8874a924df95ceda58b79
Alberto Bueno <abettetto@gmail.com>
2022-07-13 22:36:27 -0500

    Utiliza datetime para imprimir la fecha y hora actual en UTC

---
+++ Day_85.py
@@ -0,0 +1,7 @@
+''' 
+100 Dias De Python - Dia 84
+---------------------------
+Utiliza datetime para imprimir la fecha y hora actual en UTC 
+'''
+from datetime import datetime
+print(datetime.utcnow())
