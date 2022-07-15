5fb3a4995b41354fa0344d623a3ca1c9eda3bfd1
Alberto Bueno <abettetto@gmail.com>
2022-07-15 05:41:11 -0500

    Utiliza calendar para imprimir el mes de Julio en formato calendario

---
+++ Day_87.py
@@ -0,0 +1,7 @@
+''' 
+100 Dias De Python - Dia 87
+---------------------------
+Utiliza calendar para imprimir el mes de Julio en formato calendario
+'''
+import calendar
+print(calendar.month(2022,7))
