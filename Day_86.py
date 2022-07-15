dcf565da6eb65566b84d060562a3dd9485aaed4c
Alberto Bueno <abettetto@gmail.com>
2022-07-14 23:12:46 -0500

    Utiliza datetime para calcular la cantidad de segundos que han pasado desde el inicio del reto considerando solo la fecha.

--- Day_86.py
+++ Day_86.py
@@ -4,7 +4,7 @@
 Utiliza datetime para calcular la cantidad de segundos que han pasado desde el inicio del reto considerando solo la fecha. 
 Considera que el reto inicio el "20/04/2022" e imprime el resultado. 
 '''
-from datetime import datetime, date
+from datetime import datetime
 
 ini = "20/04/2022"
 fin = datetime.today().strftime('%d/%m/%Y')
