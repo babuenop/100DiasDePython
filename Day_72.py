57f4ca8de92dd38a59e557a92cb271bd8d7701a4
Alberto Bueno <abettetto@gmail.com>
2022-07-23 23:28:32 -0500

    Crea una funcion que use argumentos arbitrarios para recibir N-numeros y determine cual es el mayor y el menor  y los devuelva en un diccionario en el siguiente formato {mayor:5, menor:-10}

--- Day_72.py
+++ Day_72.py
@@ -4,8 +4,18 @@
 Utiliza itertools para obtener la cantidad de veces que se repite cada numero en la lista
 Imprime el resultado
 '''
+import itertools
+ 
 from more_itertools import unique_everseen
 
 lista = [0, 1, 1, 2, 3, 2, 4, 5, 5, 8, 4]
 result = {k:lista.count(k) for k in unique_everseen(lista)}
 print(result)
+
+# o 
+resultado={}
+data = sorted(lista)
+for k, g in itertools.groupby(data):
+    resultado[k]= len(list(g))
+print(resultado)
+    
---
+++ Day_95.py
@@ -0,0 +1,15 @@
+''' 
+100 Dias De Python - Dia 93
+---------------------------
+Crea una funcion que use argumentos arbitrarios para recibir N-numeros y determine cual es el mayor y el menor  y los devuelva en un diccionario en el siguiente formato
+{"mayor":5, "menor":-10}
+Imprime el resultado 
+'''
+
+def saludos(*args):
+    lista = [*args]
+    resultado = {"mayor": max(lista), "menor":min(lista)}
+    return(print(resultado))     
+       
+saludos(9, 10,-1,3,5,-2,8)
+
