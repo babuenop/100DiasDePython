f9322f338fa3d13fd40f6e16a1152f8cf0eedba7
Alberto Bueno <abettetto@gmail.com>
2022-07-25 23:03:39 -0500

    Crea una funcion que use argumentos arbitrarios de tipo keyword para recibir los tres lados del triangulo y calcule su perimetro. Imprime el resultado en un numero de punto flotante.

---
+++ Day_97.py
@@ -0,0 +1,12 @@
+''' 
+100 Dias De Python - Dia 96
+---------------------------
+Crea una funcion que use argumentos arbitrarios de tipo keyword para recibir los tres lados del triangulo y calcule su perimetro. 
+Imprime el resultado en un numero de punto flotante. 
+'''
+
+def perimetro(l1, l2, l3):
+    return(print(float(l1+l2+l3)))     
+       
+perimetro(l2=9,l1=10,l3=11)
+
